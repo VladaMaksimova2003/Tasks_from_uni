d0c5a9d193c37b01995525fd40cb239c103d81e6
VladaMaksimova2003 <113528642+VladaMaksimova2003@users.noreply.github.com>
2022-09-28 23:58:31 +0500

    От 26.09.2022

---
+++ 3_lab/series8.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+    int N, a, size = 0;
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    cout << "Введите число: \n";
+    cin >> N;
+    do {
+        cout << "Введите число из набора: \n";
+        cin >> a;
+        if (a % 2 == 0 && a != 0) { cout << "Число " << a << " четное" << endl; ++size; }
+        --N;
+
+    } while (N);
+    cout << "Всего четных чисел - " << size << endl;
+    system("pause");
+    //тесты: 10(0, 11, 2, 3, 4, 15, 16, 7, 18, 9), 5(11, 333, 6666, 7, 909), 5(10, 2, 12, 444, 804);
+
+}

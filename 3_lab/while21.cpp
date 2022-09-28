d6e3d7c242a91166c6e661a1045bdd08153235f4
VladaMaksimova2003 <113528642+VladaMaksimova2003@users.noreply.github.com>
2022-09-28 23:38:04 +0500

    От 26.09.2022

---
+++ 3_lab/while21.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+    int a = 0, N;
+    bool r = true;
+    cin >> N;
+    a = N % 10;
+
+    do {
+        N /= 10;
+        if (a % 2 == 1) {
+            cout << r;
+            exit(0);
+        }
+        a = N % 10;
+
+    } while (N);
+    r = false;
+    cout << r;
+    // тесты для проверки: 100, 222, 234, 122, 333, 223;
+
+}
---
+++ 3_lab/Задача_4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+void main()
+{
+
+    int random_number_comp;
+    int random_number_user;
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    do {
+        srand(time(NULL));
+        random_number_comp = rand() % 3 + 1;
+        random_number_user = 0;
+        char random[3][8] = { "Камень", "Ножницы", "Бумага" };
+        cout << "Введите Камень - 1,Ножницы - 2 или Бумага - 3" << endl;
+        cin >> random_number_user;
+        cout << "Вы ввели - " << random[random_number_user - 1] << endl << " Ввел компьютер - " << random[random_number_comp - 1] << endl;
+        if (random_number_comp == random_number_user) cout << "Ничья" << endl;
+        else if (random_number_comp == 1 && random_number_user == 3
+            || random_number_comp == 2 && random_number_user == 1
+            || random_number_comp == 3 && random_number_user == 2) cout << "Вы выиграли" << endl;
+        else cout << "Вы проиграли" << endl;
+
+
+    } while (random_number_comp == random_number_user);
+    system("pause");
+
+
+}

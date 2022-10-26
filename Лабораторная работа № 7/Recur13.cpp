7e2a4327b1fac1394a3bcadd42add0a607bfa40b
VladaMaksimova2003 <113528642+VladaMaksimova2003@users.noreply.github.com>
2022-10-27 01:26:13 +0500

    от 24.10.2022

---
+++ Лабораторная работа № 7/Recur13.cpp
@@ -0,0 +1,32 @@
+﻿//13.Описать рекурсивную функцию Palindrome(S) логического типа, 
+//возвращающую True, если строка S является палиндромом
+//(т.е.читается одинаково слева направо и справа налево), и False в противном случае.
+//Оператор цикла в теле функции не использовать.
+//Вывести значения функции Palindrome для пяти данных строк.
+#include <iostream>
+#include <string>
+using namespace std;
+bool Palindrome(string str, int size, string s, int& flag) {
+	if (size > 0) {
+		char c = str[size - 1];
+		s += c;
+		--size;
+		Palindrome(str, size, s, flag);
+	}
+	if (str == s) {
+		flag = 1; return flag;
+	}
+	return flag;
+}
+int main() {
+	string str, s = "";
+	int size, flag = 0;;
+	setlocale(LC_ALL, "ru");
+	for (int i = 0; i < 5; ++i) {
+		cout << "Введите строку ";
+		cin >> str;
+		size = str.size();
+		cout << Palindrome(str, size, s, flag) << endl;
+		flag = 0;
+	}
+}
---
+++ Лабораторная работа № 7/Recur8.cpp
@@ -0,0 +1,31 @@
+﻿//Recur8.Описать рекурсивную функцию RootK(X, K, N) вещественного типа, 
+//находящую приближенное значение корня K - й степени из числа X по формуле :
+//Y0 = 1, YN + 1 = YN −(YN − X / (YN)K−1) / K,
+//где YN обозначает RootK(X, K, N) при фиксированных X и K.Параметры функции : 
+//X(> 0) — вещественное число, K(> 1) и N(> 0) — целые.С помощью функции RootK найти для данного числа X приближенные значения его корня K - й 
+//степени при шести данных значениях N.
+#include <iostream>
+#include <cmath>
+using namespace std;
+float RootK(float X, int K, int N) {
+	if (!N) return 1;
+	return RootK(X, K, N - 1) - (RootK(X, K, N - 1) - X / pow(RootK(X, K, N - 1), K - 1)) / K;
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	int N, K;
+	float X;
+	do {
+		cout << "Введите X(> 0): ";
+		cin >> X;
+		cout << "Введите степень K(> 1): ";
+		cin >> K;
+	} while (X <= 0 || K <= 1);
+	for (int i = 0; i < 6; ++i) {
+		do {
+			cout << "Введите 6 значений N(> 0): " << endl;
+			cin >> N;
+		} while (N < 0);
+		cout << "Приближенное значение " << RootK(X, K, N) << endl;
+	}
+}

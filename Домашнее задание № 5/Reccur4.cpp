e2dfd6fbda5068daed613475fca685658cb735e8
VladaMaksimova2003 <113528642+VladaMaksimova2003@users.noreply.github.com>
2022-10-26 23:58:50 +0500

    задание от 24.10.2022

---
+++ Домашнее задание № 5/Reccur4.cpp
@@ -0,0 +1,20 @@
+﻿#include "pt4.h"
+using namespace std;
+void max(int k, int& m) {
+    int digit;
+    if (k > 0) {
+        digit = k % 10;
+        if (digit > m) m = digit;
+        max(k / 10, m);
+    }
+}
+void Solve()
+{
+    Task("Reccur4");
+    int n, m = 0;
+    pt >> n;
+    m = n % 10;
+    max(n, m);
+    pt << m;
+}
+//написать рекурсивынй алгоритм опредления максимальной цифры целого числа N
---
+++ Домашнее задание № 5/Reccur9.cpp
@@ -0,0 +1,19 @@
+﻿#include "pt4.h"
+using namespace std;
+void rec(int N, int a, float& S) {
+    if (N > 0) {
+        rec(--N, a * 2, S += a);
+    }
+}
+void Solve()
+{
+    Task("Reccur9");
+    int a = 1, N;
+    float S = 0;
+    pt >> N;
+    rec(N, a, S);
+    pt << S;
+}
+//Написать рекурсивную функцию для вычисления суммы:
+//S = 1 + 2 + 4 + 8 + 16 + ..., содержащей N первых членов
+//(N - натуральное число, N < 20, S - вещественное).

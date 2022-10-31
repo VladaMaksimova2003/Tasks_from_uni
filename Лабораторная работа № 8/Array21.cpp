86a783da9b873ef17860af6ecfdf37ae58987272
VladaMaksimova2003 <113528642+VladaMaksimova2003@users.noreply.github.com>
2022-10-31 22:53:33 +0500

    задание от 31.10.2022

---
+++ Лабораторная работа № 8/Array21.cpp
@@ -0,0 +1,29 @@
+﻿//Array21.Дан массив размера N и целые числа K и L(1 ≤ K ≤ L ≤ N).
+//Найти среднее арифметическое элементов массива с номерами от K до L включительно.
+#include <iostream>
+#include <string>
+using namespace std;
+void input(int* array, int N) {
+	for (int i = 0; i < N; ++i) {
+		cin >> array[i];
+	}
+}
+void Sum(int L, int K, int* array, int& sum) {
+	for (int i = K; i <= L; ++i) {
+		sum += array[i];
+	}
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	int L, K, N, sum = 0;
+	do {
+		cout << "Введите размер массива, значение K и значение L (1 <= K <= L <= N)соответственно" << endl;
+		cin >> N >> K >> L;
+	} while (K < 1 || L < 1 || N < 1 || K > L || K > N || L > N);
+	int* array = new int[N];
+	cout << "Введите значения в массив" << endl;
+	input(array, N);
+	Sum(L - 1, K - 1, array, sum);
+	cout << "Среднее арифметическое элементов массива с номерами от K до L включительно " << sum / (L - K + 1);
+	delete[] array;
+}
---
+++ Лабораторная работа № 8/Array90.cpp
@@ -0,0 +1,33 @@
+﻿//Array90.Дан массив размера N и целое число K(1 ≤ K ≤ N).Удалить из массива элемент с порядковым номером K.
+#include <iostream>
+#include <string>
+using namespace std;
+void input(int* array, int N) {
+	for (int i = 0; i < N; ++i) {
+		cin >> array[i];
+	}
+}
+void output(int* array, int N) {
+	for (int i = 0; i < N - 1; ++i) {
+		cout << array[i];
+	}
+}
+void Delete(int* array, int N, int K) {
+	for (int i = K; i < N - 1; ++i)
+		array[i] = array[i + 1];
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	int K, N;
+	do {
+		cout << "Введите размер массива, значение K (1 <= K <= N)" << endl;
+		cin >> N >> K;
+	} while (K < 1 || N < 1 || K > N);
+	int* array = new int[N];
+	cout << "Введите значения в массив" << endl;
+	input(array, N);
+	Delete(array, N, K - 1);
+	cout << "Массив после удаления" << endl;
+	output(array, N);
+	delete[] array;
+}
